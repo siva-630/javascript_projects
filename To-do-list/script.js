77256a2b7c496334e8f82b99c473086838832bcf
siva-630 <siva630029@gmail.com>
2026-01-30 23:27:56 +0530

    todolist

---
+++ To-do-list/script.js
@@ -0,0 +1,40 @@
+const input = document.getElementById("addtask");
+
+const addon = document.getElementById("add");
+
+const tasklist = document.getElementById("list");
+
+
+
+
+addon.addEventListener("click", () => {
+    tasklist = input.value;
+
+
+
+    if (tasklist === ""){
+        alert("enter a task");
+        return;
+
+    }
+    const li = document.createElement("li");
+    li.textContent = tasklist;
+
+    const delet = document.createElement("button");
+    delet.textContent = "❌";
+
+
+    delet.addEventListener("click", function () {
+        tasklist.removeChild(li);
+    })
+
+    li.appendChild(delet);
+
+    tasklist.appendChild(li);
+
+    input.value = " ";
+
+
+
+
+})
