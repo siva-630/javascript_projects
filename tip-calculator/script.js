c839e5b8f3d60a43c56fea993322604253ee2439
siva-630 <siva630029@gmail.com>
2026-03-03 16:49:09 +0530

    tip

---
+++ tip-calculator/script.js
@@ -0,0 +1,11 @@
+const bill = document.getElementById("billAmount");
+const tips = document.getElementById("tipPercentage");
+
+const result = document.getElementById("result");
+
+const cal = document.getElementById("calculate");
+
+
+cal.addEventListener("click", function () {
+    const bill =
+})
