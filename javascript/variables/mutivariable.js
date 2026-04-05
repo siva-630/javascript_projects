b315612e0b261c597de54d7d07cc4ad97ffad61d
siva-630 <siva630029@gmail.com>
2026-04-06 00:06:04 +0530

    mutivariable

---
+++ javascript/variables/mutivariable.js
@@ -0,0 +1,15 @@
+
+
+let a = 10, b = 23, c = 44;
+
+console.log(a,b,c);
+
+
+
+var name = "siva", age = 34, s = true;
+var name ="shankar"
+console.log(name, age, s);
+
+const animal = "tiger", food = 34, k=4;
+
+console.log(animal);
