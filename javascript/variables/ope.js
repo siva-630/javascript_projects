1f42632ef1b72cb0325a187e0c4a969aeb5d28ba
siva-630 <siva630029@gmail.com>
2026-04-05 23:50:39 +0530

    operations

---
+++ javascript/variables/ope.js
@@ -0,0 +1,20 @@
+const value1 = 5;
+const value2 = 5;
+
+console.log(value1 + value2);
+console.log(value1 - value2);
+console.log(value1 * value2);
+
+console.log(value1 / value2);
+console.log(value1 % value2);
+
+
+
+
+
+
+
+
+
+
+
