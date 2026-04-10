0196aee8b8b28f7ee3e88e21a8c60b2e51a714eb
siva-630 <siva630029@gmail.com>
2026-04-10 09:33:58 +0530

    largest

---
+++ javascript/variables/largest.js
@@ -0,0 +1,4 @@
+let a = 9, b = 55;
+
+let largest = (a > b) ? a : b;
+console.log(largest);
---
+++ javascript/variables/rectangle.js
@@ -0,0 +1,9 @@
+
+
+function a(length, breath, height) {
+    
+    console.log(length * breath * height);
+    
+    
+}
+a(1,2,3);
